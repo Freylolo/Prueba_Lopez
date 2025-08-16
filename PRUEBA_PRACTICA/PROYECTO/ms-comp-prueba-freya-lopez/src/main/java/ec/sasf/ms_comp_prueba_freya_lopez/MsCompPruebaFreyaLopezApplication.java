b1ab6cf4e7bfe36fb9a7930b8da3babe45687fed
Freya López <freylolo20@outlook.com>
2025-08-16 18:01:21 -0500

    Inicial commit

---
+++ PRUEBA_PRACTICA/PROYECTO/ms-comp-prueba-freya-lopez/src/main/java/ec/sasf/ms_comp_prueba_freya_lopez/MsCompPruebaFreyaLopezApplication.java
@@ -0,0 +1,13 @@
+package ec.sasf.ms_comp_prueba_freya_lopez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCompPruebaFreyaLopezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCompPruebaFreyaLopezApplication.class, args);
+	}
+
+}
---
+++ PRUEBA_PRACTICA/PROYECTO/ms-comp-prueba-freya-lopez/src/test/java/ec/sasf/ms_comp_prueba_freya_lopez/MsCompPruebaFreyaLopezApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.sasf.ms_comp_prueba_freya_lopez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCompPruebaFreyaLopezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
