871f975678a2687158e0d3323ea6604f2418c924
Freya López <freylolo20@outlook.com>
2025-08-16 20:10:20 -0500

    service

--- PRUEBA_PRACTICA/PROYECTO/ms-comp-prueba-freya-lopez/src/main/java/ec/sasf/ms_comp_prueba_freya_lopez/MsCompPruebaFreyaLopezApplication.java
+++ PRUEBA_PRACTICA/PROYECTO/ms-comp-prueba-freya-lopez/src/main/java/ec/sasf/ms_comp_prueba_freya_lopez/MsCompPruebaFreyaLopezApplication.java
@@ -2,7 +2,11 @@ package ec.sasf.ms_comp_prueba_freya_lopez;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
+@EnableJpaAuditing
 @SpringBootApplication
 public class MsCompPruebaFreyaLopezApplication {
 
--- PRUEBA_PRACTICA/PROYECTO/ms-comp-prueba-freya-lopez/src/main/java/ec/sasf/ms_comp_prueba_freya_lopez/service/ProductoService.java
+++ PRUEBA_PRACTICA/PROYECTO/ms-comp-prueba-freya-lopez/src/main/java/ec/sasf/ms_comp_prueba_freya_lopez/service/ProductoService.java
@@ -1,4 +1,23 @@
 package ec.sasf.ms_comp_prueba_freya_lopez.service;
 
+import ec.sasf.ms_comp_prueba_freya_lopez.persistence.entity.ProductoEntity;
+import ec.sasf.ms_comp_prueba_freya_lopez.persistence.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductoService {
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public ProductoEntity crearProducto(ProductoEntity producto) {
+        return productoRepository.save(producto);
+    }
+
+    public List<ProductoEntity> listaProductos() {
+        return productoRepository.findByDisponibleTrue();
+    }
+
 }
